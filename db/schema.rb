3bd1f182f95b7740a28d4778aaf0b13f2ea329c9
Erik <3rikthered.main@gmail.com>
2019-07-20 12:27:53 -0400

    generated estimates db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_143844) do
+ActiveRecord::Schema.define(version: 2019_07_20_161745) do
+
+  create_table "estimates", force: :cascade do |t|
+    t.integer "worst"
+    t.integer "best"
+    t.integer "expected"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "project_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.text "project"
