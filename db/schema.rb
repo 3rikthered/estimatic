0ce288c7c4d0c4962a6b9b5b3bff71322fd02e05
Erik <3rikthered.main@gmail.com>
2019-07-20 10:48:14 -0400

    Added name and teams columns to user db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_121509) do
+ActiveRecord::Schema.define(version: 2019_07_20_143844) do
 
   create_table "projects", force: :cascade do |t|
     t.text "project"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2019_07_20_121509) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "team"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
